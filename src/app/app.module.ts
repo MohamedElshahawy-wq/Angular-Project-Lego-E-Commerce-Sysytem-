ccfa93186d25ef8d9a821d2d2a16b7885dbb6b64
alaa <alaamohammedsaif@gmail.com>
2021-01-22 20:52:53 +0200

    adding header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { VipComponent } from './components/vip/vip.component';
 import { FindStoreComponent } from './components/find-store/find-store.component';
 import { LegoLifeComponent } from './components/lego-life/lego-life.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VipComponent,
     FindStoreComponent,
-    LegoLifeComponent
+    LegoLifeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
