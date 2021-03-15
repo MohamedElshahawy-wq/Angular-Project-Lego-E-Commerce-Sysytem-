188017b603d8ac5483de8615466c43fbe1558942
Nour El Deen Khaled <nourkhaled15@hotmail.com>
2021-03-16 00:18:46 +0200

    Fixed links

--- src/app/components/find-store/find-store.component.ts
+++ src/app/components/find-store/find-store.component.ts
@@ -14,12 +14,33 @@ export class FindStoreComponent implements OnInit {
   map: any;
 
   ngOnInit() {
-    
+
   }
-// 
-viewStore() {
-  if(this.findStoreSearch.nativeElement.value == 'egypt')
-  {
+  // 
+  viewStore() {
+    if (this.findStoreSearch.nativeElement.value == 'egypt') {
+      this.map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([22.0, 37.0]),
+          zoom: 5
+        })
+      });
+      console.log("value: ", this.findStoreSearch.nativeElement.value);
+      var view = this.map.getView();
+      view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+      view.setZoom(5);
+    }
+    else {
+      this.viewAll();
+    }
+  }
+  viewAll() {
     this.map = new ol.Map({
       target: 'map',
       layers: [
@@ -28,36 +49,14 @@ viewStore() {
         })
       ],
       view: new ol.View({
-        center: ol.proj.fromLonLat([22.0, 37.0]),
-        zoom: 5
+        center: ol.proj.fromLonLat([22.5204, 73.8500]),
+        zoom: 8
       })
     });
-  console.log("value: ",this.findStoreSearch.nativeElement.value);
-  var view = this.map.getView();
-  view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
-  view.setZoom(5);
+    // 
+    console.log("value: ", this.findStoreSearch.nativeElement.value);
+    var view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([73.8500, 22.5204]));
+    // view.setZoom(8);
   }
-  else{
-    this.viewAll();
-  }
-}
-viewAll(){
-  this.map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([22.5204, 73.8500]),
-      zoom: 8
-    })
-  });
-  // 
-  console.log("value: ",this.findStoreSearch.nativeElement.value);
-  var view = this.map.getView();
-  view.setCenter(ol.proj.fromLonLat([ 73.8500, 22.5204]));
-  // view.setZoom(8);
-}
 }
